862a8e4e8bb2b3e445d43a97b90c089dfb5bae93
Sai Kiran <kirann9679@icloud.com>
2023-03-31 16:35:13 -0500

    Added DoubleInteger kata

---
+++ src/main/java/CodeWars/Java.java
@@ -0,0 +1,10 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//Solution
+package CodeWars;
+    public class Java {
+        public static int doubleInteger(int i) {
+            i=i+i;
+            return i;
+        }
+    }
